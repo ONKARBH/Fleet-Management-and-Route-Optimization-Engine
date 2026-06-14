8a2130e3e918040021ff2be879e606613b83d6c4
Onkar Bhosale <your_email@gmail.com>
2026-06-14 15:44:40 +0530

    added cros origin

--- Fleet-Backend/Fleet-Management-System/src/main/java/com/fleet_management/controller/DeliveryTaskController.java
+++ Fleet-Backend/Fleet-Management-System/src/main/java/com/fleet_management/controller/DeliveryTaskController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173"})
 @RestController
 @RequestMapping("/api/deliveries")
 public class DeliveryTaskController {
--- Fleet-Backend/Fleet-Management-System/src/main/java/com/fleet_management/controller/DispatchController.java
+++ Fleet-Backend/Fleet-Management-System/src/main/java/com/fleet_management/controller/DispatchController.java
@@ -11,6 +11,7 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173"})
 @RestController
 @RequestMapping("/api/dispatch")
 public class DispatchController {
